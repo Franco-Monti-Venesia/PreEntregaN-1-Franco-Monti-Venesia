da7e50861793e55bcab29a090738f6eff8946f3a
Franco Monti Venesia <francovenesia05@gmail.com>
2025-09-23 13:15:09 -0300

    Agregado del while y las opciones de pastas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
 const pastas = ["Fideos", "Ñoquis", "Ravioles", "Sorrentinos"];
-const precios = [4000, 4400, 7000, 8000];
+const precios = [4000, 4400, 7500, 8000];
 
 let continuar = true;
+
+while (continuar) {
+    let opcionPastas = parseInt(
+        prompt("Elija una pasta:\n 1: Fideos   2: Ñoquis   3: Ravioles   4: Sorrentinos   5: Salir")
+    );
+
+    switch (opcionPastas) {
+        case 1: 
+            alert("Elegiste Fideos. El precio es: $4000");
+            console.log("Elegiste Fideos. El precio es de:: $4000");
+            break;
+        case 2: 
+            alert("Elegiste Ñoquis. El precio es: $4500");
+            console.log("Elegiste Ñoquis. El precio es de: $4500");
+            break;
+        case 3:
+            alert("Elegiste Ravioles. El precio es de: $7500");
+            console.log("Elegiste Ravioles. El precio es de: $7500");
+            break;
+        case 4:
+            alert("Elegiste Sorrentinos. El precio es de: $8000");
+            console.log("Elegiste Sorrentinos. El precio es de: $8000");
+            break;
+        case 5:
+            alert("Gracias por visitar la fábrica de pastas");
+            continuar = false; 
+            break;
+        default:
+            alert("Selecciona una opcion que sea correcta")
+    }
+}
