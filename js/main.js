c8edba6206223cee5d633de01ae2b15f1aee6a4d
Franco Monti Venesia <francovenesia05@gmail.com>
2025-09-23 21:00:31 -0300

    metiendo la logica en las funciones con parametro

--- js/main.js
+++ js/main.js
@@ -2,44 +2,58 @@ const pastas = ["Fideos", "Ñoquis", "Ravioles", "Sorrentinos"];
 const precios = [4000, 4400, 7500, 8000];
 
 
+function elegirPasta() {
+    return parseInt(
+        prompt("Que tipo de pasta quiere?\n 1: Fideos   2: Ñoquis   3: Ravioles   4: Sorrentinos   5: Salir")
+    );
+}
+
+
+function obtenerPrecio(opcion) {
+    let mensaje = "";
+    switch (opcion) {
+        case 1:
+            mensaje = "Elegiste Fideos. El precio es 4000";
+            break;
+        case 2:
+            mensaje = "Elegiste Ñoquis. El precio es 4400";
+            break;
+        case 3:
+            mensaje = "Elegiste Ravioles. El precio es 7500";
+            break;
+        case 4:
+            mensaje = "Elegiste Sorrentinos. El precio es 8000";
+            break;
+        case 5:
+            mensaje = "Gracias por visitar la fábrica de pastas";
+            break;
+        default:
+            mensaje = "Selecciona una opción correcta";
+    }
+    return mensaje;
+}
+
+
+function mostrarResultado(mensaje) {
+    alert(mensaje);
+    console.log(mensaje);
+}
+
+
 let continuar = true;
 
 while (continuar) {
-    let opcionPastas = parseInt(
-        prompt("Elija una pasta:\n 1: Fideos   2: Ñoquis   3: Ravioles   4: Sorrentinos   5: Salir")
-    );
+    let opcionPastas = elegirPasta();
+    let mensaje = obtenerPrecio(opcionPastas);
+    mostrarResultado(mensaje);
 
-    switch (opcionPastas) {
-        case 1: 
-            alert("Elegiste Fideos. El precio es: $4000");
-            console.log("Elegiste Fideos. El precio es de:: $4000");
-            break;
-        case 2: 
-            alert("Elegiste Ñoquis. El precio es: $4500");
-            console.log("Elegiste Ñoquis. El precio es de: $4500");
-            break;
-        case 3:
-            alert("Elegiste Ravioles. El precio es de: $7500");
-            console.log("Elegiste Ravioles. El precio es de: $7500");
-            break;
-        case 4:
-            alert("Elegiste Sorrentinos. El precio es de: $8000");
-            console.log("Elegiste Sorrentinos. El precio es de: $8000");
-            break;
-        case 5:
-            alert("Gracias por visitar la fábrica de pastas");
-            continuar = false; 
-            break;
-        default:
-            alert("Selecciona una opcion que sea correcta")
-    }
-
-
-    if (continuar) {
-        let seguir = prompt("Quiere algo más?  si/no");
+    if (opcionPastas === 5) {
+        continuar = false;
+    } else {
+        let seguir = prompt("¿Quiere algo más? si/no");
         if (seguir === "no") {
             continuar = false;
-            alert("Gracias por su compra")
+            alert("Gracias por su compra");
         }
     }
 }
